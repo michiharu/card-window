049e978babab759e94d4ecfdfe600b1221002069
michiharu <michi1diver@gmail.com>
2021-12-18 03:07:45 +0900

    :shirt: fix rollup.config.js

--- packages/card-window/rollup.config.js
+++ packages/card-window/rollup.config.js
@@ -1,22 +1,13 @@
-import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json";
+import typescript from 'rollup-plugin-typescript2';
+
+import pkg from './package.json';
 
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: [
-    {
-      file: "./lib/cjs/index.js",
-      format: "cjs",
-    },
-    {
-      file: "./lib/esm/index.js",
-      format: "es",
-    },
+    { file: './lib/cjs/index.js', format: 'cjs' },
+    { file: './lib/esm/index.js', format: 'es' },
   ],
   external: [...Object.keys(pkg.peerDependencies || {})],
-  plugins: [
-    typescript({
-      typescript: require("typescript"),
-    }),
-  ],
+  plugins: [typescript()],
 };
