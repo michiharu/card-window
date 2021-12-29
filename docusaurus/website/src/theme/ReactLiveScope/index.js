830c10e7edbe1a38d906077e6c0cc7af001265d2
michiharu <michi1diver@gmail.com>
2021-12-29 18:32:38 +0900

    :books: fix styling and typo

--- docusaurus/website/src/theme/ReactLiveScope/index.js
+++ docusaurus/website/src/theme/ReactLiveScope/index.js
@@ -8,8 +8,8 @@
 import React from 'react';
 import { CardWindow } from 'card-window';
 
-const Card = ({ data, index, style }) => (
-  <div className="card" style={{ ...style, height: style.height - 16, backgroundColor: '#fff' }}>
+const Card = ({ index, style }) => (
+  <div className="card" style={{ ...style, height: style.height - 16 }}>
     <div className="card__body">
       <h2>{index}</h2>
     </div>
