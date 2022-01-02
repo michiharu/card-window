f275c11c264adac31a6025008d3e338e39c8c505
michiharu <michi1diver@gmail.com>
2022-01-03 08:21:30 +0900

    :books: fix

--- docusaurus/website/src/components/HomepageFeatures.tsx
+++ docusaurus/website/src/components/HomepageFeatures.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
+import { CardWindow } from 'card-window';
 
 type FeatureItem = {
   title: string;
@@ -11,18 +12,20 @@ type FeatureItem = {
 const FeatureList: FeatureItem[] = [
   {
     title: 'Easy to Use',
-    image: '/img/undraw_docusaurus_mountain.svg',
+    image: '/card-window/img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Card Window requires only 3 props.<br />
-        And it has simple option props.<br />
+        Card Window requires only 3 props.
+        <br />
+        And it has simple option props.
+        <br />
         Check out the tutorial right away!
       </>
     ),
   },
   {
     title: 'Automatically Adjust Columns',
-    image: '/img/undraw_docusaurus_tree.svg',
+    image: '',
     description: (
       <>
         The card window uses <code>ResizeObserver</code> internally. Therefore, the column is automatically adjusted
@@ -32,15 +35,57 @@ const FeatureList: FeatureItem[] = [
   },
   {
     title: 'Inspired by "react-window"',
-    image: '/img/undraw_docusaurus_react.svg',
+    image: '/card-window/img/undraw_docusaurus_react.svg',
     description: (
       <>
-        It works by only rendering part of a large data set (just enough to fill the viewport).
-        It is very much inspired by Brian Vaughn's react-window.
+        It works by only rendering part of a large data set (just enough to fill the viewport). It is very much inspired
+        by Brian Vaughn's react-window.
       </>
     ),
   },
 ];
+
+const range = (_start: number, _end?: number): number[] => {
+  const start = _end === undefined ? 0 : _start;
+  const end = _end ?? _start;
+  const list: number[] = [];
+  for (let i = start; i < end; i += 1) list.push(i);
+  return list;
+};
+
+const seed = (int: number) => Math.floor(Math.random() * int);
+
+function FeatureDemo({ title, image, description }: FeatureItem) {
+  const ref = React.useRef(range(29).map(() => seed(30)));
+  
+  return (
+    <div className={clsx('col col--4')}>
+      <div
+        className={styles.demo}
+        style={{ marginTop: 20, marginBottom: 20, marginLeft: 'auto', marginRight: 'auto' }}
+      >
+        <CardWindow
+          data={ref.current}
+          cardRect={{ width: 80, height: 80 }}
+          style={{ height: 160 }}
+          innerStyle={{ background: 'linear-gradient(65deg, #F13F79, #2196f3)' }}
+          lastRowAlign="left"
+        >
+          {({ data, index, style }) => (
+            <div className="card" style={style}>
+              <img alt="docusaurus" src={`https://picsum.photos/seed/${data[index]}/60/60`} />
+            </div>
+          )}
+        </CardWindow>
+      </div>
+
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
 
 function Feature({ title, image, description }: FeatureItem) {
   return (
@@ -61,9 +106,9 @@ export default function HomepageFeatures(): JSX.Element {
     <section className={styles.features}>
       <div className="container">
         <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+          <Feature {...FeatureList[0]} />
+          <FeatureDemo {...FeatureList[1]} />
+          <Feature {...FeatureList[2]} />
         </div>
       </div>
     </section>
