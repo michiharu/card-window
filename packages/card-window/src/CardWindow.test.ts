e6e248fe886621bd3932499449fc62a6ff8d1f02
michiharu <michi1diver@gmail.com>
2022-01-04 12:49:10 +0900

    :green_heart: update test for changing cols logic

--- packages/card-window/src/CardWindow.test.ts
+++ packages/card-window/src/CardWindow.test.ts
@@ -1,5 +1,7 @@
 import { functions } from './CardWindow';
 
+import { Spacing } from '.';
+
 const {
   getColumns,
   // getScrollContainerStyle,
@@ -13,23 +15,40 @@ const {
 
 describe('getColumns', () => {
   const height = 0;
-  const y = 0;
-  test('container.width < card.width', () => {
-    expect(getColumns({ width: 100, height }, { width: 101, height }, { x: 0, y })).toBe(0);
-  });
-  test('1 column patterns', () => {
-    expect(getColumns({ width: 100, height }, { width: 100, height }, { x: 0, y })).toBe(1);
-    expect(getColumns({ width: 100, height }, { width: 90, height }, { x: 0, y })).toBe(1);
-    expect(getColumns({ width: 100, height }, { width: 90, height }, { x: 10, y })).toBe(1);
-    expect(getColumns({ width: 100, height }, { width: 100, height }, { x: 10, y })).toBe(1);
-    expect(getColumns({ width: 200, height }, { width: 100, height }, { x: 10, y })).toBe(1);
-  });
+  const spacing = { y: 0, top: 0, bottom: 0 };
 
-  test('many columns patterns', () => {
-    expect(getColumns({ width: 200, height }, { width: 90, height }, { x: 10, y })).toBe(2);
-    expect(getColumns({ width: 200, height }, { width: 60, height }, { x: 20, y })).toBe(2);
-    expect(getColumns({ width: 200, height }, { width: 60, height }, { x: 11, y })).toBe(2);
-    expect(getColumns({ width: 200, height }, { width: 60, height }, { x: 10, y })).toBe(3);
+  describe('justifyContent: not space-evenly', () => {
+    const justifyContent = 'center';
+    test('container.width < card.width', () => {
+      expect(getColumns({ width: 100, height }, { width: 101, height }, { ...spacing, x: 0 }, justifyContent)).toBe(0);
+    });
+    test('1 column patterns', () => {
+      expect(getColumns({ width: 100, height }, { width: 100, height }, { ...spacing, x: 0 }, justifyContent)).toBe(1);
+      expect(getColumns({ width: 100, height }, { width: 90, height }, { ...spacing, x: 0 }, justifyContent)).toBe(1);
+      expect(getColumns({ width: 100, height }, { width: 90, height }, { ...spacing, x: 10 }, justifyContent)).toBe(1);
+      expect(getColumns({ width: 100, height }, { width: 100, height }, { ...spacing, x: 10 }, justifyContent)).toBe(1);
+      expect(getColumns({ width: 200, height }, { width: 100, height }, { ...spacing, x: 10 }, justifyContent)).toBe(1);
+    });
+
+    test('many columns patterns', () => {
+      expect(getColumns({ width: 200, height }, { width: 90, height }, { ...spacing, x: 10 }, justifyContent)).toBe(2);
+      expect(getColumns({ width: 200, height }, { width: 60, height }, { ...spacing, x: 20 }, justifyContent)).toBe(2);
+      expect(getColumns({ width: 200, height }, { width: 60, height }, { ...spacing, x: 11 }, justifyContent)).toBe(2);
+      expect(getColumns({ width: 200, height }, { width: 60, height }, { ...spacing, x: 10 }, justifyContent)).toBe(3);
+    });
+  });
+  describe('justifyContent: space-evenly', () => {
+    const justifyContent = 'space-evenly';
+    test('container.width < card.width', () => {
+      expect(getColumns({ width: 100, height }, { width: 101, height }, { ...spacing, x: 0 }, justifyContent)).toBe(0);
+    });
+    test('1 column: 200 < 90 * 2 + 10 * 3', () => {
+      expect(getColumns({ width: 200, height }, { width: 90, height }, { ...spacing, x: 10 }, justifyContent)).toBe(1);
+    });
+
+    test('2 columns pattern: 200 < 80 * 2 + 10 * 3', () => {
+      expect(getColumns({ width: 200, height }, { width: 80, height }, { ...spacing, x: 10 }, justifyContent)).toBe(2);
+    });
   });
 });
 
@@ -38,41 +57,42 @@ describe('getRenderContainerStyle', () => {
   const x = 0;
   const justifyContent = 'space-evenly';
   test('row === 0, card.height: 100, spacing.y: 0', () => {
-    const style = getRenderContainerStyle(0, { width, height: 100 }, { x, y: 0 }, justifyContent);
-    expect(style.transform).toBe('translate(0, 0px)');
+    const style = getRenderContainerStyle(0, { width, height: 100 }, { x, y: 0, top: 10, bottom: 0 }, justifyContent);
+    expect(style.transform).toBe('translate(0, 10px)');
   });
   test('row === 0, card.height: 100, spacing.y: 10', () => {
-    const style = getRenderContainerStyle(0, { width, height: 100 }, { x, y: 10 }, justifyContent);
-    expect(style.transform).toBe('translate(0, 0px)');
+    const style = getRenderContainerStyle(0, { width, height: 100 }, { x, y: 10, top: 10, bottom: 0 }, justifyContent);
+    expect(style.transform).toBe('translate(0, 10px)');
   });
   test('row === 1, card.height: 100, spacing.y: 0', () => {
-    const style = getRenderContainerStyle(1, { width, height: 100 }, { x, y: 0 }, justifyContent);
-    expect(style.transform).toBe('translate(0, 100px)');
+    const style = getRenderContainerStyle(1, { width, height: 100 }, { x, y: 0, top: 10, bottom: 0 }, justifyContent);
+    expect(style.transform).toBe('translate(0, 110px)');
   });
   test('row === 1, card.height: 100, spacing.y: 10', () => {
-    const style = getRenderContainerStyle(1, { width, height: 100 }, { x, y: 10 }, justifyContent);
-    expect(style.transform).toBe('translate(0, 110px)');
+    const style = getRenderContainerStyle(1, { width, height: 100 }, { x, y: 10, top: 10, bottom: 0 }, justifyContent);
+    expect(style.transform).toBe('translate(0, 120px)');
   });
 });
 
 describe('getRenderRowRange', () => {
   const width = 0;
-  const x = 0;
   test('container.height: 100, card.height: 100, spacing.y: 0', () => {
-    expect(getRenderRowRange(0, 0, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([0, 1]);
-    expect(getRenderRowRange(0, 1, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([0, 2]);
-    expect(getRenderRowRange(0, 50, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([0, 2]);
-    expect(getRenderRowRange(0, 51, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([0, 3]);
-    expect(getRenderRowRange(1000, 0, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([10, 11]);
-    expect(getRenderRowRange(1000, 1, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([9, 11]);
-    expect(getRenderRowRange(1000, 50, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([9, 11]);
-    expect(getRenderRowRange(1000, 51, { width, height: 100 }, { width, height: 100 }, { x, y: 0 })).toEqual([9, 12]);
+    const spacing: Spacing = { x: 0, y: 0, top: 0, bottom: 0 };
+    expect(getRenderRowRange(0, 0, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([0, 1]);
+    expect(getRenderRowRange(0, 1, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([0, 2]);
+    expect(getRenderRowRange(0, 50, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([0, 2]);
+    expect(getRenderRowRange(0, 51, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([0, 3]);
+    expect(getRenderRowRange(1000, 0, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([10, 11]);
+    expect(getRenderRowRange(1000, 1, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([9, 11]);
+    expect(getRenderRowRange(1000, 50, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([9, 11]);
+    expect(getRenderRowRange(1000, 51, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([9, 12]);
   });
   test('container.height: 100, card.height: 100, spacing.y: 50', () => {
+    const spacing: Spacing = { x: 0, y: 50, top: 0, bottom: 0 };
     // 1000 / (100 + 50) = 6.66
-    expect(getRenderRowRange(1000, 0, { width, height: 100 }, { width, height: 100 }, { x, y: 50 })).toEqual([6, 7]);
+    expect(getRenderRowRange(1000, 0, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([6, 7]);
     // (100 + 50 * 2) / (100 + 50) + 1 = 3
-    expect(getRenderRowRange(1000, 50, { width, height: 100 }, { width, height: 100 }, { x, y: 50 })).toEqual([6, 8]);
+    expect(getRenderRowRange(1000, 50, { width, height: 100 }, { width, height: 100 }, spacing)).toEqual([6, 8]);
   });
 });
 
